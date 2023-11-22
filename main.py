7fa85f425efde73ab517f2cb76d9849b567b35aa
B2ng4 <125038202+B2ng4@users.noreply.github.com>
2023-11-22 18:11:09 +1000

    Добавление таблицы

--- main.py
+++ main.py
@@ -5,27 +5,38 @@ from dash import dash_table
 from dash import html
 from dash import dcc
 import plotly.graph_objs as go
+import dash
+from dash import html
+from dash import dcc
 import pandas as pd
+from dash.dash_table.Format import Group
 
-# Данные
+
+# Создаем данные для таблицы
+data = {'Показатель': ['Общее прибыль', 'Чистый профит', 'Реальная прибыль', 'Проfit/ОТМ'],
+        'Значение': [100000, 50000, 30000, 15]}
+df = pd.DataFrame(data)
+
+# Создаем данные для круговой диаграммы
 labels = ['Категория 1', 'Категория 2', 'Категория 3', 'Категория 4', 'Категория 5']
 values = [35, 20, 15, 22, 12]
 
-data = {'date': ['2022-01-01', '2022-02-01', '2022-03-01', '2022-04-01', '2022-05-01'],
+# Создаем данные для линейного графика
+line_data = {'date': ['2022-01-01', '2022-02-01', '2022-03-01', '2022-04-01', '2022-05-01'],
         'value': [10, 20, 30, 25, 35]}
-df = pd.DataFrame(data)
-df['date'] = pd.to_datetime(df['date'])
+line_df = pd.DataFrame(line_data)
+line_df['date'] = pd.to_datetime(line_df['date'])
 
 # Создаем Dash приложение
 app = dash.Dash(__name__)
 
 # Определяем макет приложения
 app.layout = html.Div([
-    html.H1('Линейный график'),
+    html.H1('Финансовые показатели'),
     dcc.Graph(
         figure={
             'data': [
-                go.Scatter(x=df['date'], y=df['value'], mode='lines+markers', name='Временной ряд')
+                {'x': line_df['date'], 'y': line_df['value'], 'type': 'scatter', 'mode': 'lines+markers', 'name': 'Временной ряд'}
             ],
             'layout': {
                 'title': 'График расходов и доходов',
@@ -34,7 +45,6 @@ app.layout = html.Div([
             }
         }
     ),
-    
     dcc.Graph(
         figure={
             'data': [go.Pie(labels=labels, values=values)],
@@ -42,8 +52,20 @@ app.layout = html.Div([
                 'title': 'Круговая диаграмма'
             }
         }
-    )
+    ),
+    html.Div([
+        html.H1('Таблица с данными'),
+        dash_table.DataTable(
+            columns=[{'id': 'Показатель', 'name': 'Показатель'}, {'id': 'Значение', 'name': 'Значение'}],
+            data=df.to_dict('records'),
+            style_table={'border': '1px solid black'},
+            style_header={'backgroundColor': 'rgb(20, 20, 20)', 'color': 'white'},
+            style_cell={'textAlign': 'center', 'backgroundColor': 'rgb(50, 50, 50)', 'color': 'white'}
+        )
+    ])
 ])
 
+# Запускаем приложение
 if __name__ == '__main__':
     app.run_server(debug=True)
+
